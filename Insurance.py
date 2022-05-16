25cd8ba1150569e00281e23ef87d1e97757ee9b6
ranapradip935 <104645231+ranapradip935@users.noreply.github.com>
2022-05-17 01:14:57 +0530

    Update Insurance.py

--- Insurance.py
+++ Insurance.py
@@ -117,5 +117,5 @@ final_features = [np.array(int_features)]
 if st.button('Predict'):           # when the submit button is pressed
     prediction =  loaded_model.predict(final_features)
     st.balloons()
-    st.success(f'Your insurance charges would be: ${round(prediction[0],2)}')
+    st.success('Your insurance charges would be dollar:',round(prediction[0],2))
     
